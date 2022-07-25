d2b35f177cccd6b71ceb5548d73282cb1262cbb3
excal.rs <91800528+Excal-rs@users.noreply.github.com>
2022-07-25 22:28:29 +0100

    Update prime-checker.py

--- prime-checker.py
+++ prime-checker.py
@@ -1,10 +1,8 @@
 import math
-import sys
-import time
 
 n = 1
 
-#user interface
+#user input
 while True:
     try:
         num = int(input("Please enter a integer you want check for being prime: "))
@@ -15,16 +13,20 @@ while True:
 
 max_num = math.ceil(math.sqrt(num))
 #makes it more efficient by making it only need to try fewer numbers
+if num == 1 or num == 4:
+    print(f"{num} is not prime")
+    #deals with how 1 and 4 did not work with this system
 
-for count in range(2, max_num):
-    n += 2
-# ensures number is always even
-    if num % n == 0:
-        print(f"{num} is not prime.")
-        break
-    elif num % 2 == 0:
-        print(f"{num} is not prime.")
-        break
 else:
-    print(f"{num} is prime")
-#output
+    for count in range(2, max_num):
+        n += 2
+    # ensures number is always odd
+        if num % n == 0:
+            print(f"{num} is not prime.")
+            break
+        elif num % 2 == 0:
+            print(f"{num} is not prime.")
+            break
+    else:
+        print(f"{num} is prime")
+    #output
