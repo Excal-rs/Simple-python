754703561da476dfc13acd81594c81818d4b103b
excal.rs <91800528+Excal-rs@users.noreply.github.com>
2022-07-26 15:41:45 +0100

    Create palindrom-checker.py

---
+++ palindrom-checker.py
@@ -0,0 +1,14 @@
+string = str(input("Please enter a phrase or word to check if it is a palindrome: \n"))
+
+def rmSpaces(s):
+    s = s.replace(' ', '')
+    return s
+# removes spaces for phrases e.g race car
+
+string2 = rmSpaces(string)
+
+if string2[::-1].lower() == string2.lower():
+    print(f"Guess what! {string},  is a palindrome! :)")
+else:
+    print(f"{string}, is not a palindrome. :(")
+    #output
