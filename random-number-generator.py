d0a99bd2c5e118ef8c657256b7418ffee16927a4
excal.rs <91800528+Excal-rs@users.noreply.github.com>
2022-07-26 13:58:45 +0100

    Create random-number-generator.py

---
+++ random-number-generator.py
@@ -0,0 +1,21 @@
+import random
+
+while True:
+    try:
+        a = int(input("What would you like the lower bound for the random number generator to be? "))
+        break
+    except ValueError:
+        print("Error: Please enter a valid integer! \n")
+        # asks for lower bound while ensuring the program is not stopped by a invalid input e.g "hello"
+
+while True:
+    try:
+        b = int(input("What would you like the upper bound for the random number generator to be? "))
+        break
+    except ValueError:
+        print("Error: Please enter a valid integer! \n")
+        # asks for upper bound while ensuring the program is not stopped by a invalid input e.g "hello"
+
+num = random.randint(a,b)
+print(num)
+#output
